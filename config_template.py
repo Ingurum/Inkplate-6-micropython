c3385ba18969e58ecf1f48d02691d0267f8161a0
Rahul Ravikumar <tikurahul@gmail.com>
2020-12-25 00:34:41 -0800

    Add a config template

---
+++ config_template.py
@@ -0,0 +1,21 @@
+# Copy this file and rename it to `config.py`
+
+# OAuth2 Configuration
+# Look at: https://developers.google.com/identity/protocols/oauth2/limited-input-device
+
+CLIENT_ID = '<your_client_id>'
+CLIENT_SECRET = '<your_client_secret>'
+DISCOVERY_ENDPOINT = 'https://accounts.google.com/.well-known/openid-configuration'
+
+# The  list of OAuth2 scopes. 
+SCOPES = [
+    'openid',
+    'https://www.googleapis.com/auth/calendar.readonly'
+]
+
+# The location for auth state serialization and deserialization.
+SAVED_LOCATION =  'tokens.json'
+
+# Wireless Config
+WLAN_SSID = '<your_ssid>'
+WLAN_PASSWORD = '<your_password>'
